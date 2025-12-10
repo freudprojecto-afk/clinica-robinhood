522140e0dc035a1b6ac52232d4b8bfd4b9e5435d
freudprojecto-afk <freudprojecto@gmail.com>
2025-12-10 20:27:29 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
@@ -8,5 +9,6 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
 
 
