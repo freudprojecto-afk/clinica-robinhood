7f36660e6a3208b8e8df95b239f4e1ddf37c1840
freudprojecto-afk <freudprojecto@gmail.com>
2025-12-26 19:50:11 +0000

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         // Nova paleta de cores - Clínica Freud
         'clinica-bg': '#f2f2f0',        // Fundo neutro (não branco)
         'clinica-primary': '#355a6c',    // Cabeçalho, títulos, menus, rodapé
-        'clinica-text': '#1f2a30',      // Texto corrido, subtítulos
+        'clinica-text': '#1f2a30',      // Texto corrido, subtítulos (quase preto)
         'clinica-accent': '#e6ded3',    // Submenu, blocos empáticos, testemunhos
         'clinica-menu': '#a68a62',      // Menus (dourado acastanhado)
         'clinica-cta': '#f5b746',       // CTAs (amarelo dourado)
